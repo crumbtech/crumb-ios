a35d51b06c90ab297e69d8e489005c6ea2c490f8
ross creighton <rosson.creighton@gmail.com>
2017-04-14 03:31:18 -0400

    adds a super basic validator function

--- src/components/AuthForm/validator.js
+++ src/components/AuthForm/validator.js
@@ -1,3 +1,9 @@
 export default values => {
-  return {};
+  const errors = {};
+  const requiredText = 'this field is required';
+  if (!values.firstName) errors.firstName = requiredText;
+  if (!values.lastName) errors.lastName = requiredText;
+  if (!values.phoneNumber) errors.phoneNumber = requiredText;
+  if (!values.password) errors.password = requiredText;
+  return errors;
 };
